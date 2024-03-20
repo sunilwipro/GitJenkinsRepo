2a678b0bc8f1e5f18a9a02cfd2887268b6894f8a
sunilwipro <33525580+sunilwipro@users.noreply.github.com>
2024-03-20 06:06:33 +0000

    Add files via upload
    Uploaded the file odeven.java

---
+++ Odevn.java
@@ -0,0 +1,24 @@
+public class Odevn
+{
+
+public static void main(String [] args)
+{
+int a=13;
+if(a%2==0)
+{
+System.out.println("Even");
+}
+else
+
+{
+System.out.println("ODD");
+}
+
+for(int i =0; i<=a;i++)
+{
+System.out.println("Value is "+i);
+}
+
+}
+
+}
