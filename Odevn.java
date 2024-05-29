a2e1ecaf9b91392952b3a87cc2c260c2a6dec7bc
sunilwipro <33525580+sunilwipro@users.noreply.github.com>
2024-05-29 10:29:34 +0000

    Update Odevn.java

--- Odevn.java
+++ Odevn.java
@@ -2,13 +2,13 @@ public class Odevn
 {
 public static void main(String [] args)
 {
-int a=13;
+int a=12;
 if(a%2==0)
 {
 System.out.println("Even");
+System.out.println("Wipro");
 }
 else
-
 {
 System.out.println("ODD");
 }
